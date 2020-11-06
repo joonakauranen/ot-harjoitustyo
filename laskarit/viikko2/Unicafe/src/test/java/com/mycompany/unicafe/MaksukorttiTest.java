ca4fd596b83dcb7636e3bd4eb0c56fd5a88eb66f
Joona <joona.kauranen@helsinki.fi>
2020-11-06 17:44:12 +0200

    laskarit viikko2 tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,49 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(10000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 100.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(5000);
+        assertEquals("saldo: 150.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortiltaOttaminenToimiiOikein() {
+        kortti.otaRahaa(5000);
+        assertEquals("saldo: 50.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortiltaOttaminenEiMahdollistaJosEiTarpeeksiSaldoa() {
+        kortti.otaRahaa(10001);
+        assertEquals("saldo: 100.0", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTrueJosSaldoRiittaa() {
+        assertTrue(kortti.otaRahaa(9999));
+    }
+    
+    @Test
+    public void palauttaaFalseJosSaldoEiRiita() {
+        assertFalse(kortti.otaRahaa(10001));
+    }
+    
+    @Test
+    public void saldoMetodiPalauttaaOikeanSaldon() {
+        assertEquals(10000, kortti.saldo());
     }
 }
